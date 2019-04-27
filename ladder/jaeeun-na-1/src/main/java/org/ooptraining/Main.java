04bc5ed7a268d47306edb01a00e883d86da6a257
Jae Eun Na <lette1394@gmail.com>
2019-04-27 17:23:38 +0900

    Fix Main test glitches

--- ladder/jaeeun-na-1/src/main/java/org/ooptraining/Main.java
+++ ladder/jaeeun-na-1/src/main/java/org/ooptraining/Main.java
@@ -14,7 +14,7 @@ public class Main {
         final List<String> messages = Arrays.asList(
                 "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)",
                 "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)",
-                "최대 사다리 높이는 몇 개인가요?\n"
+                "최대 사다리 높이는 몇 개인가요?"
         );
         final List<String> resultMessages = Arrays.asList(
                 "사다리 결과",
@@ -45,6 +45,7 @@ public class Main {
             final String input = sc.nextLine();
             container.add(input);
         });
+        System.out.println();
         resultMessages.forEach(m -> {
             System.out.println(m);
         });
