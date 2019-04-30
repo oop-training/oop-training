9a81b981ed75d9993e3904bec0f1028494fb7f81
Jae Eun Na <lette1394@gmail.com>
2019-05-01 01:54:55 +0900

    Add more test for SHOW query command, exception with wrong command
    DisplayName을 더욱 의미 있게 수정
    IDE의 도움을 받기 위해서 MethodSource로 필드를 주었던 것을 하드코딩으로 변경
    SHOW command에 대한 테스트를 작성하는데, MethodSource로 테스트에 필요한 정보를 모두 받음
    command 종류가 더 추가되면 주석 처리된 테스트를 진행

--- ladder/jaeeun-na-1/src/test/java/org/ooptraining/query/QueryProcessorTest.java
+++ ladder/jaeeun-na-1/src/test/java/org/ooptraining/query/QueryProcessorTest.java
@@ -2,25 +2,28 @@ package org.ooptraining.query;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.ooptraining.exception.IllegalQueryCommandException;
+import org.ooptraining.query.argument.ShowQueryCommandArgument;
 import org.ooptraining.setting.SettingContext;
+import org.ooptraining.util.dummy.data.SettingContextDummys;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.ooptraining.util.StringUtils.makeFullResultWithNameAndResult;
 
 @DisplayName("QueryProcessor")
 class QueryProcessorTest {
-    private static final String source1 = "org.ooptraining.util.dummy.data.SettingContextDummys#data1";
-
     @Nested
     @DisplayName("can handle")
     class Handle {
 
         @DisplayName("SHOW_ALL command")
-        @ParameterizedTest(name = "{arguments}-{index}")
-        @MethodSource(source1)
-        void query_test_1(final SettingContext settingContext) {
+        @ParameterizedTest(name = "[{index}]-{arguments}")
+        @MethodSource("org.ooptraining.util.dummy.data.SettingContextDummys#data1")
+        void query_test_show_all_command(final SettingContext settingContext) {
             final QueryProcessor queryProcessor = QueryProcessor.of(settingContext);
 
             final QueryResult queryResult = queryProcessor.execute(QueryCommand.SHOW_ALL);
@@ -28,5 +31,44 @@ class QueryProcessorTest {
             final String expected = makeFullResultWithNameAndResult(settingContext);
             assertThat(queryResult.asString()).isEqualTo(expected);
         }
+
+        @DisplayName("SHOW command (specific name-result)")
+        @ParameterizedTest(name = "[{index}]-{arguments}")
+        @MethodSource("org.ooptraining.util.dummy.data.SettingContextDummys#participantList_queryName_expectedResult")
+        void query_test_show_command(final SettingContext settingContext, final String queryName, final String expectedResult) {
+            final QueryProcessor queryProcessor = QueryProcessor.of(settingContext);
+            final QueryArgument queryArgument = ShowQueryCommandArgument.of(queryName);
+
+            final QueryResult queryResult = queryProcessor.execute(QueryCommand.SHOW, queryArgument);
+
+            assertThat(queryResult.asString()).isEqualTo(expectedResult);
+        }
+    }
+
+    @Nested
+    @DisplayName("should throw exception")
+    class ThrowException {
+
+        @Test
+        @DisplayName("when execute UNKNOWN command")
+        void wrong_command() {
+            final SettingContext settingContext = SettingContextDummys.simpleOne();
+            final QueryProcessor queryProcessor = QueryProcessor.of(settingContext);
+
+            assertThrows(IllegalQueryCommandException.class, () -> {
+                queryProcessor.execute(QueryCommand.UNKNOWN);
+            });
+        }
+
+//        @Test
+//        @DisplayName("when execute valid command with wrong query arguments")
+//        void valid_command_with_wrong_query_arguments() {
+//            final SettingContext settingContext = SettingContextDummys.simpleOne();
+//            final QueryProcessor queryProcessor = QueryProcessor.of(settingContext);
+//
+//            assertThrows(IllegalArgumentException.class, () -> {
+//                queryProcessor.execute(QueryCommand.SHOW, null);
+//            });
+//        }
     }
 }
--- ladder/jaeeun-na-1/src/test/java/org/ooptraining/util/dummy/data/SettingContextDummys.java
+++ ladder/jaeeun-na-1/src/test/java/org/ooptraining/util/dummy/data/SettingContextDummys.java
@@ -1,5 +1,6 @@
 package org.ooptraining.util.dummy.data;
 
+import org.junit.jupiter.params.provider.Arguments;
 import org.ooptraining.Participant;
 import org.ooptraining.setting.SettingContext;
 
@@ -7,6 +8,18 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class SettingContextDummys {
+    public static SettingContext simpleOne() {
+        return SettingContext.builder()
+                .participants(Arrays.asList(
+                        Participant.of("abcd", "O"),
+                        Participant.of("000", "X"),
+                        Participant.of("123", "X"),
+                        Participant.of("456", "X"),
+                        Participant.of("789", "X")
+                        )
+                ).maxHeight(8).build();
+    }
+
     public static Stream<SettingContext> data1() {
         return Stream.of(
                 SettingContext.builder()
@@ -41,6 +54,59 @@ public class SettingContextDummys {
                                 Participant.of("123", "456")
                                 )
                         ).maxHeight(10).build()
+        );
+    }
+
+    public static Stream<Arguments> participantList_queryName_expectedResult() {
+        return Stream.of(
+                Arguments.of(
+                        SettingContext.builder()
+                                .participants(Arrays.asList(
+                                        Participant.of("abcd", "O"),        // target
+                                        Participant.of("000", "X"),
+                                        Participant.of("123", "X"),
+                                        Participant.of("456", "X"),
+                                        Participant.of("789", "X")
+                                        )
+                                ).maxHeight(8).build(),
+                        "abcd",
+                        "O"
+                ),
+
+                Arguments.of(
+                        SettingContext.builder()
+                                .participants(Arrays.asList(
+                                        Participant.of("hello", "world"),
+                                        Participant.of("im", "groot"),      // target
+                                        Participant.of("1234", "X")
+                                        )
+                                ).maxHeight(4).build(),
+                        "im",
+                        "groot"
+
+                ),
+                Arguments.of(
+
+                        SettingContext.builder()
+                                .participants(Arrays.asList(
+                                        Participant.of("pobi", "X"),
+                                        Participant.of("Jaeeun", "Na"),
+                                        Participant.of("what", "what"),     // target
+                                        Participant.of("1", "1")
+                                        )
+                                ).maxHeight(10).build(),
+                        "what",
+                        "what"
+                ),
+                Arguments.of(
+                        SettingContext.builder()
+                                .participants(Arrays.asList(
+                                        Participant.of("123", "456")        // target
+                                        )
+                                ).maxHeight(10).build(),
+                        "123",
+                        "456"
+                )
         );
     }
 }
