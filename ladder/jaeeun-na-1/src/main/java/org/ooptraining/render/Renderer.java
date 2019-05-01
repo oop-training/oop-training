8c571136d003ed99ff7eae78e5629a8544693ca2
Jae Eun Na <lette1394@gmail.com>
2019-05-01 21:31:06 +0900

    Change to use inner method in Renderer

--- ladder/jaeeun-na-1/src/main/java/org/ooptraining/render/Renderer.java
+++ ladder/jaeeun-na-1/src/main/java/org/ooptraining/render/Renderer.java
@@ -1,18 +1,15 @@
 package org.ooptraining.render;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
 import org.ooptraining.setting.SettingContext;
 
-@Log
 @RequiredArgsConstructor(staticName = "of")
 public class Renderer {
     private final RenderPolicy renderPolicy;
-
     private RenderContext renderContext = RenderContext.DEFAULT;
 
     public String render(final SettingContext settingContext) {
-        return renderPolicy.render(settingContext, renderContext);
+        return render(settingContext, renderContext);
     }
 
     public String render(final SettingContext settingContext, final RenderContext renderContext) {
@@ -24,6 +21,3 @@ public class Renderer {
         return this;
     }
 }
-
-// TODO: RenderContext가 하는 일을 Renderer가 받도록 하자.
-//  클라이언트는 Renderer에 설정을 넣는 방식으로 사용할 수 있도록 하자. (RenderContext는 감추고, 내부에서 생성)
