61ef935e30cc605e8765c35f0e5926733a90d5c7
Jae Eun Na <lette1394@gmail.com>
2019-05-01 20:36:18 +0900

    Rename QueryProcessorTest -> QueryCommandTest
    내가 생각한 QueryProcessor는
    
    1. 사용자의 입력을 받고
    2. 적절한 QueryCommand를 생성해서
    3. 각 Command에 맞는 결과를 요청하고
    4. 결과를 반환하는
    
    애였는데, QueryProcessor가 QueryCommand가 하는 일을 그대로 해버렸다.

---
+++ ladder/jaeeun-na-1/src/test/java/org/ooptraining/query/QueryCommandTest.java
@@ -0,0 +1,59 @@
+package org.ooptraining.query;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.ooptraining.exception.IllegalQueryCommandException;
+import org.ooptraining.query.argument.ShowQueryCommandArgument;
+import org.ooptraining.setting.SettingContext;
+import org.ooptraining.util.dummy.data.SettingContextDummys;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.ooptraining.util.StringUtils.makeFullResultWithNameAndResult;
+
+@DisplayName("QueryCommand")
+class QueryCommandTest {
+    @Nested
+    @DisplayName("can handle")
+    class Handle {
+
+        @DisplayName("SHOW_ALL command")
+        @ParameterizedTest(name = "[{index}]-{arguments}")
+        @MethodSource("org.ooptraining.util.dummy.data.SettingContextDummys#data1")
+        void query_test_show_all_command(final SettingContext settingContext) {
+            final QueryResult queryResult = QueryCommand.SHOW_ALL.execute(settingContext);
+
+            final String expected = makeFullResultWithNameAndResult(settingContext);
+            assertThat(queryResult.asString()).isEqualTo(expected);
+        }
+
+        @DisplayName("SHOW command (specific name-result)")
+        @ParameterizedTest(name = "[{index}]-{arguments}")
+        @MethodSource("org.ooptraining.util.dummy.data.SettingContextDummys#participantList_queryName_expectedResult")
+        void query_test_show_command(final SettingContext settingContext, final String queryName, final String expectedResult) {
+            final QueryArgument queryArgument = ShowQueryCommandArgument.of(queryName);
+
+            final QueryResult queryResult = QueryCommand.SHOW.execute(settingContext, queryArgument);
+
+            assertThat(queryResult.asString()).isEqualTo(expectedResult);
+        }
+    }
+
+    @Nested
+    @DisplayName("should throw exception")
+    class ThrowException {
+
+        @Test
+        @DisplayName("when execute UNKNOWN command")
+        void wrong_command() {
+            final SettingContext settingContext = SettingContextDummys.simpleOne();
+
+            assertThrows(IllegalQueryCommandException.class, () -> {
+                QueryCommand.UNKNOWN.execute(settingContext);
+            });
+        }
+    }
+}
